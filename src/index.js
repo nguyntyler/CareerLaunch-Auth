9c56a1e6b6c16ea546d2d6b9c58a572e1b222c9b
Tyler Nguyen <nguyn.tyler@gmail.com>
2021-03-19 16:13:19 -0400

    created first post request

--- src/index.js
+++ src/index.js
@@ -4,13 +4,20 @@ const helmet = require("helmet");
 const port = 4000;
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(helmet());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+	res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+	const { name } = req.body;
+	res.send(name);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+	console.log(`Example app listening at http://localhost:${port}`);
 });
